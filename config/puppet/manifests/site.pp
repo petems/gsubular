864d2a393b90a3e576209b87545e91b08f4c53e9
petems <p.morsou@gmail.com>
2013-08-15 17:52:15 +0100

    Unless causing headaches...

--- config/puppet/manifests/site.pp
+++ config/puppet/manifests/site.pp
@@ -17,6 +17,5 @@ class { ruby: version => '2.0.0-p0' }
 
 exec {'gem-install-bundler':
   command =>'/usr/bin/gem install bundler',
-  unless  =>'/usr/bin/gem which bundler',
   require =>Class['ruby'],
 }
